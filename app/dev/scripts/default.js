379de225fc6ede4015df823dd359e0b554f2f84d
Henning Meyer <h.meyer@affiliprint.de>
2014-10-21 11:27:56 +0200

    default now in jquery instead of plain javascript

--- app/dev/scripts/default.js
+++ app/dev/scripts/default.js
@@ -1,17 +1,17 @@
 /* Ruft getPositionPointerXY auf um die Positions des Cursors in die Textarea zu schreiben sobald das Event onmousemove auftritt */
-document.onmousemove = getPositionPointerXY;
+$(document).mousemove(getPositionPointerXY);
 
 function getPositionPointerXY (event) {
 	/* Ermitteln der Position des Mouse Cursors */
-	var xPosition = document.all ? event.offsetX : event.pageX;
-	var yPosition = document.all ? event.offsetY : event.pageY;
+	var xPosition = event.pageX;
+	var yPosition = event.pageY;
 
 	/* Output der Funktionen wird in einer Variable gespeichert */
 	var positionOutput = 'Position: ' + xPosition + ', ' + yPosition;
 
 	/* Auswählen der Textarea in dem die Position angezeigt wird */
-	var textareaPointer = document.getElementById('pointer-textarea');
+	var textareaPointer = $('#pointer-textarea');
 
 	/* Ausgabe der Position in der eben ausgewählten Textarea */
-	textareaPointer.value = positionOutput;
+	textareaPointer[0].value = positionOutput;
 }
