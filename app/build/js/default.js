826e8ac09dc0420fccb44e6e5208e90925281992
Henning Meyer <h.meyer@affiliprint.de>
2014-10-28 16:21:53 +0100

    default.js is now generated with the coffee dist task

--- app/build/js/default.js
+++ app/build/js/default.js
@@ -1,21 +1,21 @@
-/* Ruft getPositionPointerXY auf um die Positions des Cursors in die Textarea zu schreiben sobald das Event onmousemove auftritt */
-$(document).mousemove(getPositionPointerXY);
+(function() {
+  var getPositionPointerXY;
 
-function getPositionPointerXY (event) {
-	/* Ermitteln der Position des Mouse Cursors */
-	var xPosition = event.pageX;
-	var yPosition = event.pageY;
+  $(document).mousemove(function(event) {
+    return getPositionPointerXY(event);
+  });
 
-	/* Output der Funktionen wird in einer Variable gespeichert */
-	var positionOutput = 'Position: ' + xPosition + ', ' + yPosition;
+  getPositionPointerXY = function(event) {
+    var positionOutput, textareaPointer, xPosition, yPosition;
+    xPosition = event.pageX;
+    yPosition = event.pageY;
+    positionOutput = 'Position: ' + xPosition + ', ' + yPosition;
+    textareaPointer = $('#pointer-textarea');
+    textareaPointer[0].value = positionOutput;
+    return $(".background-div").css({
+      top: yPosition + "px",
+      left: xPosition + "px"
+    });
+  };
 
-	/* Auswählen der Textarea in dem die Position angezeigt wird */
-	var textareaPointer = $('#pointer-textarea');
-
-	/* Ausgabe der Position in der eben ausgewählten Textarea */
-	textareaPointer[0].value = positionOutput;
-
-	/* Positioniert das Div direkt am Mauszeiger */
-	$('.background-div').css({'top': yPosition + 'px',
-							'left' : xPosition + 'px'});
-}
+}).call(this);
